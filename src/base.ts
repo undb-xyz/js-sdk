8d7841b3fafa2127356c936c203cf1bebdb1293c
nichenqin <nichenqin@hotmail.com>
2023-10-21 09:04:18 +0800

    add base client

---
+++ src/base.ts
@@ -0,0 +1,21 @@
+import fetch from 'isomorphic-fetch'
+import { IUndbSDKConfig } from './config'
+
+export abstract class BaseClient {
+  private readonly url: string
+  private readonly apiKey: string
+
+  constructor(config: IUndbSDKConfig) {
+    this.url = config.baseURL || 'http://localhost:3000'
+    this.apiKey = config.apiKey
+  }
+
+  protected request(method: string /** TODO: method types */) {
+    return fetch(this.url, {
+      method,
+      headers: {
+        'x-undb-api-key': this.apiKey,
+      },
+    })
+  }
+}
---
+++ src/config.ts
@@ -0,0 +1,26 @@
+export interface IUndbSDKConfig {
+  /**
+   * undb base url
+   *
+   * @default http://localhost:3000
+   */
+  baseURL?: string
+
+  /**
+   * undb api key
+   *
+   * @see {@link https://docs.undb.xyz/openapi/1tokens/}
+   * @memberof UndbSDKConfig
+   */
+  apiKey: string
+
+  /**
+   * http header
+   *
+   * @example
+   * {
+   * 	"Content-Type": "application/json"
+   * }
+   */
+  headers: Record<string, string>
+}
